a974630bcc07a00fe50dc5072b251c381af77fc9
Leonardo Manriquez <leonardo.manriquez@healthatom.com>
2025-04-29 09:09:12 -0400

    first commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?
+
+function is_public_folder_available(): bool
+{
+    return true;
+}
+
+function get_public_folder_path(): string
+{
+    if (!is_public_folder_available()) {
+        throw new Exception("Public folder is not available");
+    }
+
+    return "/public";
+}
